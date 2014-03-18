ad7d88e7b800c013a1b6be0bc680f0a28edb012f
joelSimpson <joel_simpson_41@hotmail.com>
2014-03-17 22:07:42 -0400

    heuristic 3?

--- symmetric-bricks/board.h
+++ symmetric-bricks/board.h
@@ -3,6 +3,7 @@
 
 #include "brick.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Board
@@ -14,14 +15,27 @@ public:
 	void writeToOutput(ofstream &);
 	int heuristic1();
 	int heuristic2();
+	int heuristic3();
 	int findClosestSameColourBrick(int x, int y);
+	Brick* makeRandomMove(Brick* previous);
+	vector<Brick*> Board::getAvailableLegalMoves();
+	bool move(Brick* newPosition);
+	bool doesRowHaveMoreThanHalf(int row);
+	int* getEmptyBrickPosition();
 
 	static const int BOARD_HEIGHT = 3;
 	static const int BOARD_WIDTH = 5;
 	int totalMoves;
+	int colourCount;
 	string currentOutputFile;
 
 	Brick *emptyBrick;
+
+	int blues;
+	int reds;
+	int whites;
+	int greens;
+	int yellows;
 
 	bool moveUp();
 	bool moveRight();
